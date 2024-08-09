bf63a1dd1226b0cd62e9e5d047cc5358638008a8
Baldomero <baldomero.napoli@gmail.com>
2024-08-08 20:12:07 -0400

    se elimina .kotlin

--- shared/src/commonMain/kotlin/com/baldomero/napoli/supabase/auth/di/platformModule.kt
+++ shared/src/commonMain/kotlin/com/baldomero/napoli/supabase/auth/di/platformModule.kt
@@ -1,29 +1,5 @@
 package com.baldomero.napoli.supabase.auth.di
 
-import com.baldomero.napoli.supabase.auth.data.repositories.AuthRepositoryImpl
-import com.baldomero.napoli.supabase.auth.domain.repository.AuthRepository
-import com.baldomero.napoli.supabase.auth.domain.usecases.CheckIsLoggedUserUseCase
-import com.baldomero.napoli.supabase.auth.domain.usecases.CreateUseWithEmailAndPasswordUseCase
-import com.baldomero.napoli.supabase.auth.domain.usecases.LoginWithGoogleUseCase
-import com.baldomero.napoli.supabase.auth.domain.usecases.SignInWithEmailAndPasswordUseCase
 import org.koin.core.module.Module
-import org.koin.dsl.module
 
 expect fun platformModule(): Module
-
-@Suppress("unused")
-object DependencyInjection {
-
-    private fun repositoryModule() = module {
-        single<AuthRepository> { AuthRepositoryImpl(get()) }
-    }
-    private fun useCaseModule() = module {
-        single { CreateUseWithEmailAndPasswordUseCase(get()) }
-        single { SignInWithEmailAndPasswordUseCase(get()) }
-        single { CheckIsLoggedUserUseCase(get()) }
-        single { LoginWithGoogleUseCase(get()) }
-    }
-    fun init(): Module = module {
-        includes(platformModule(), repositoryModule(), useCaseModule())
-    }
-}
